e89e9cb3e45a3e474ccacd307a8363ee9e8bcea2
Mahesh <maheshr8484@gmail.com>
2020-12-13 10:30:04 +0530

    Initial commit

---
+++ app/src/main/java/com/motionlayoutdemo/mrsquare/Activitys/DemoActivity.kt
@@ -0,0 +1,12 @@
+package com.motionlayoutdemo.mrsquare.Activitys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.motionlayoutdemo.mrsquare.R
+
+class DemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo)
+    }
+}
